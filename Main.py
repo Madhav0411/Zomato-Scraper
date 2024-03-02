686aeefddd73573ffefc6ff0254ec2402857a126
Madhav0411 <madhavso2018@gmail.com>
2024-03-02 21:27:05 +0530

    02-03-2024

--- Main.py
+++ Main.py
@@ -23,42 +23,45 @@ for category in categories:
     time.sleep(180)
 
     soup = BeautifulSoup(driver.page_source, "html.parser")
-    hotels = ["https://www.zomato.com"+hotel.find("a")['href'] for hotel in soup.find_all("div",{"class":"jumbo-tracker"})]
+    hotels = ["https://www.zomato.com"+hotel.find("a")['href'] for hotel in soup.find_all("div",{"class":"sc-hAcydR"})]
     print(hotels)
 
     data = []
     page = 1
     total_hotels = len(hotels)
     for hotel in hotels:
-        print(f"Hotel no. {page} out of {total_hotels} Hotels in Category - {category}")
-        new_data = {}
-        driver.get("/".join(hotel.split("/")[:5]))
-        time.sleep(2)
-        soup = BeautifulSoup(driver.page_source, "html.parser")
-        name = soup.find("h1",{"class":"sc-7kepeu-0 sc-iSDuPN fwzNdh"}).text
-        location = soup.find("p",{"class":"sc-1hez2tp-0 clKRrC"}).text
-        dining_rating = soup.find_all("div",{"class":"sc-1q7bklc-1 cILgox"})[0].text
-        delivery_rating = soup.find_all("div",{"class":"sc-1q7bklc-1 cILgox"})[1].text
+        try:
+            print(f"Hotel no. {page} out of {total_hotels} Hotels in Category - {category}")
+            new_data = {}
+            driver.get("/".join(hotel.split("/")[:5]))
+            time.sleep(2)
+            soup = BeautifulSoup(driver.page_source, "html.parser")
+            name = soup.find("h1",{"class":"sc-7kepeu-0 sc-iSDuPN fwzNdh"}).text
+            location = soup.find("p",{"class":"sc-1hez2tp-0 clKRrC"}).text
+            dining_rating = soup.find_all("div",{"class":"sc-1q7bklc-1 cILgox"})[0].text
+            delivery_rating = soup.find_all("div",{"class":"sc-1q7bklc-1 cILgox"})[1].text
 
 
-        driver.get("/".join(hotel.split("/")[:-1])+"/order")
-        time.sleep(2)
-        soup = BeautifulSoup(driver.page_source,"html.parser")
-        items = soup.find_all("div",{"class":"sc-1s0saks-11 cYGeYt"})
-        for item in items:
-            item_name = item.find("h4",{"class":"sc-1s0saks-15 iSmBPS"}).text
-            item_price = item.find("span",{"class":"sc-17hyc2s-1 cCiQWA"}).text
-            
-            new_data["Name"] = name
-            new_data["Location"] = location
-            new_data["Dining Ratings"] = dining_rating
-            new_data["Delivery Ratings"] = delivery_rating
-            new_data["Item Name"] = item_name
-            new_data["Item Price"] = item_price
-            new_data["Restaurant URL"] = hotel
+            driver.get("/".join(hotel.split("/")[:-1])+"/order")
+            time.sleep(2)
+            soup = BeautifulSoup(driver.page_source,"html.parser")
+            items = soup.find_all("div",{"class":"sc-1s0saks-11 cYGeYt"})
+            for item in items:
+                item_name = item.find("h4",{"class":"sc-1s0saks-15 iSmBPS"}).text
+                item_price = item.find("span",{"class":"sc-17hyc2s-1 cCiQWA"}).text
+                
+                new_data["Name"] = name
+                new_data["Location"] = location
+                new_data["Dining Ratings"] = dining_rating
+                new_data["Delivery Ratings"] = delivery_rating
+                new_data["Item Name"] = item_name
+                new_data["Item Price"] = item_price
+                new_data["Restaurant URL"] = hotel
 
-            data.append(new_data)
-        page += 1
+                data.append(new_data)
+            page += 1
+        except:
+            continue
 
     df = pd.DataFrame(data)
     df.to_excel("Data/"+url.split("/")[3]+"-"+category+".xlsx", index=False)
